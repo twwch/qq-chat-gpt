a5f667e5f8b43cf7d62d60464502e73351acf912
陈毫 <chenhao03@xiaoduoai.com>
2023-03-04 18:00:47 +0800

    fix: 更换镜像

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
+docker rm -f qq-robot-chatgpt
+docker run -it -m 1800m  --restart=always -d --net=host --name qq-robot-chatgpt \
+        -v /etc/hosts:/etc/hosts \
+        -v /var/qq-robot/:/bots \
+        -v /etc/localtime:/etc/localtime \
+        -e LANG="en_US.UTF-8" \
+        twwch/qq-chat-gpt:master
 
