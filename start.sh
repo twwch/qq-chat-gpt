f8fe01a559308e5a53bc0e0b707f46234442987f
陈毫 <chenhao03@xiaoduoai.com>
2023-03-05 10:14:56 +0800

    fix: 修改配置

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ docker run -it -m 1800m  --restart=always -d --net=host --name qq-robot-chatgpt 
         -v /var/qq-robot/:/app/bots \
         -v /etc/localtime:/etc/localtime \
         -e LANG="en_US.UTF-8" \
+        -e BOT_PATH='file:/app/bots/*.yaml' \
         twwch/qq-chat-gpt:master
 
